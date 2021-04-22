25e8a27cf4c6f9454fe10126c661bdc6c07f7c94
paulinaeb <pdespejo18@gmail.com>
2021-04-21 21:28:07 -0400

    estilos added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { rejects } = require('assert');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry:'./src/index.js',
@@ -30,5 +31,9 @@ module.exports = {
                 filename:'./index.html',
             }
         ),
+        new CopyWebpackPlugin({
+            patterns: [{ from: './src/styles/styles.css',
+            to: '' }],
+        })
     ]
 }
